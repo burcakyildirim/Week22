15cbfc622179e182471ba793d49cae8860f893de
burcakyildirim <brckyldrm@gmail.com>
2024-02-13 17:53:16 +0300

    first commit

---
+++ sportCenter/src/components/Header.jsx
@@ -0,0 +1,29 @@
+const Header = () => {
+  return (
+    <>
+     <header>
+      <nav className="navbar-container">
+        <div className="logo">
+          <img src="./img/logo.png" alt="band" />
+        </div>
+        <input type="checkbox" className="menu-btn" id="menu-btn" />
+        <label htmlFor="menu-btn" className="menu-icon">
+          <i className="fa-solid fa-bars menu-hmbgr"></i>
+        </label>
+        <div className="bar">
+          <a href="#home">Home</a>
+          <a href="#classes">Classes</a>
+          <a href="#trainer">Trainer</a>
+          <a href="#review">Review</a>
+          <a href="#contact">Contact</a>
+          <button>
+            <a href="#joinus"><p>JOIN US</p></a>
+          </button>
+        </div>
+      </nav>
+    </header>
+    </>
+  )
+}
+
+export default Header
